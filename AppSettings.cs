f356603c2f9f2aaf076a6213f10a12ea6c90c80c
ignackoo <107203036+ignackoo@users.noreply.github.com>
2022-07-23 11:38:01 +0200

    Add files via upload

---
+++ AppSettings.cs
@@ -0,0 +1,31 @@
+﻿/******************************************************************************
+**  Copyright(c) 2022 ignackoo. All rights reserved.
+**
+**  Licensed under the MIT license.
+**  See LICENSE file in the project root for full license information.
+**
+**  This file is a part of the C# Library AppSettings.
+** 
+**  Read Application settings parameter values
+**
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Library
+{
+    public class AppSettings
+    {
+        /// <summary>
+        /// Get AppSettings parameter value.
+        /// </summary>
+        /// <param name="key">Key parameter.</param>
+        /// <returns>Value parameter.</returns>
+        public static string Get(string key)
+        { 
+            return (System.Configuration.ConfigurationManager.AppSettings[key]);
+        }
+    }
+}
